138d79fa6b6a4a137fc531cbde39388fb9ab367d
Spencer Stafford <spencestafford2187@gmail.com>
2024-10-18 11:48:00 -0700

    App should run now and be linked to a db.

--- back-end/config.py
+++ back-end/config.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #this initializes the flask i think?
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})# this would allow for integration with a React app, as theyre both hosted on different ports
+CORS(app) #this is working in pycharm for me, use the other one in VSCode
+#CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})# this would allow for integration with a React app, as theyre both hosted on different ports
 # Flask is hosted on port 5000, React is hosted on port 3000
 
 # these are the links to the db. postgresql://<user>:<password>@<host>/<database_name>
